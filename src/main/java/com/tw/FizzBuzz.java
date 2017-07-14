1c8c4ef31ace6add7f1ad94c49e95807536ecc41
puwang <puwang@thoughtworks.com>
2017-07-14 15:23:00 +0800

    rafactor: remove FizzBuzzMatcher

--- src/main/java/com/tw/FizzBuzz.java
+++ src/main/java/com/tw/FizzBuzz.java
@@ -8,8 +8,7 @@ import static java.util.Collections.sort;
 public class FizzBuzz {
     private final BuzzMatcher buzzMatcher = new BuzzMatcher();
     private final FizzMatcher fizzMatcher = new FizzMatcher();
-    private final FizzBuzzMatcher fizzBuzzMatcher = new FizzBuzzMatcher(fizzMatcher, buzzMatcher);
-    private List<Matcher> matchers = Arrays.asList(buzzMatcher, fizzBuzzMatcher, fizzMatcher);
+    private List<Matcher> matchers = Arrays.asList(buzzMatcher, fizzMatcher);
 
     public FizzBuzz() {
         //noinspection unchecked
@@ -17,12 +16,13 @@ public class FizzBuzz {
     }
 
     public String count(int number) {
+        String result = "";
         for (Matcher matcher : matchers) {
             if (matcher.isMatched(number)) {
-                return matcher.getText();
+                result += matcher.getText();
             }
         }
-        return String.valueOf(number);
+        return result.isEmpty() ? String.valueOf(number) : result;
     }
 
 }
